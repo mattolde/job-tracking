48028f146d8b9cdedae82d7163028ca6798d5665
Matt Olde <mattolde@gmail.com>
2015-03-20 19:47:48 +1100

    added basic mongo to app

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     stylus = require('stylus'),
     logger = require('morgan'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -34,6 +35,30 @@ app.use(stylus.middleware(
 // Setup static file location for express will serve the file when requested.
 app.use(express.static(__dirname + '/public'));
 
+
+// Mongo setup
+
+mongoose.connect('mongodb://localhost/jobs');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function() {
+  console.log('jobs db opened');
+});
+
+var messageSchema = mongoose.Schema({
+  message: String
+});
+
+var Message = mongoose.model('Message', messageSchema);
+
+var mongoMessage;
+
+Message.findOne().exec(function(err, messageDoc){
+  mongoMessage = messageDoc.message;
+});
+
+
+
 app.get('/partials/:partialPath', function(req, res) {
   res.render('partials/' + req.params.partialPath);
 });
@@ -42,7 +67,9 @@ app.get('/partials/:partialPath', function(req, res) {
 // ROUTES
 
 app.get('*', function(req, res) {
-  res.render('index');
+  res.render('index', {
+    mongoMessage: mongoMessage
+  });
 });
 
 
