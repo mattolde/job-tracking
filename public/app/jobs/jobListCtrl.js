36d084fb1a9d8805c1b13d5bb5356682d35a8c4c
Matt Olde <mattolde@gmail.com>
2015-03-23 19:58:29 +1100

    added search and filter to jobs list

--- public/app/jobs/jobListCtrl.js
+++ public/app/jobs/jobListCtrl.js
@@ -2,4 +2,22 @@ angular.module('app')
 
 .controller('jobListCtrl',['$scope', 'jobSrv', function($scope, jobSrv) {
   $scope.jobs = jobSrv.query();
+
+  $scope.sortOptions = [
+    {
+      value: 'title',
+      text: 'Sort by Title'
+    },
+    {
+      value: 'company',
+      text: 'Sort by Company'
+    },
+    {
+      value: 'date',
+      text: 'Sort by Date'
+    }
+  ];
+
+  $scope.sortOrder = $scope.sortOptions[0].value;
+
 }]);
